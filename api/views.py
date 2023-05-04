45626ce487769f7a3b42a7a0aea6fa922dae8484
hunzo <takraw_s@hotmail.com>
2023-05-04 15:19:58 +0700

    first commit

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view ,permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def Home(request):
+    return Response({
+        "status": True
+    })
